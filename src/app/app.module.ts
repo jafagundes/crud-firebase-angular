627036bb5419fc5a2ea9e2a6383267d0db648fcd
jonathan <fagundes.ja@gmail.com>
2018-09-17 21:18:37 -0300

    Configuração inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    AddTasksComponent
   ],
   imports: [
     BrowserModule
