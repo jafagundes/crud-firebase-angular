e66305b9f18f4ace247dc3aaab7d67da70807dff
jonathan.fagundes <jonathan.fagundes@codeitsolutions.com.br>
2018-09-21 10:52:29 -0300

    Ajustes finais.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
 
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { TaskService } from './services/task.service';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDReWAAkG8WbIFKkG783BsJRKx-QaEePAM",
+    authDomain: "angular-crud-76374.firebaseapp.com",
+    databaseURL: "https://angular-crud-76374.firebaseio.com",
+    projectId: "angular-crud-76374",
+    storageBucket: "angular-crud-76374.appspot.com",
+    messagingSenderId: "339855808925"
+  }
 };
